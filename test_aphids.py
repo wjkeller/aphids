f8ee0c4dbb431c4c7126261ac97715770bcbb323
Rory Neithinger <rory.trent@gmail.com>
2015-07-19 23:59:42 -0700

    add test stubs

--- test_aphids.py
+++ test_aphids.py
@@ -17,6 +17,24 @@ def test_version(client):
     assert json.loads(rv.data.decode()) == {'version': '0.1.0'}
 
 
+def test_welcome(client):
+    rv = client.get('/')
+    assert rv.status_code == 200
+    assert b'<a href="/authenticate">authenticate</a>' in rv.data
+    assert b'<a href="/register">create account</a>' in rv.data
+    assert b'good' in rv.data; assert b'justice' in rv.data
+
+
+def test_authenticate(client):
+    rv = client.get('/authenticate')
+    assert rv.status_code == 200
+
+
+def test_register(client):
+    rv = client.get('/register')
+    assert rv.status_code == 200
+
+
 @pytest.fixture
 def client():
     return aphids.app.test_client()
